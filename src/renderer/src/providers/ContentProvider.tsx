10c2d7851f6d0aa28be3a571f5586a4614f4835e
Vitu2002 <contact@vitu2002.dev>
2025-03-10 16:42:24 +0100

    fix(ipc): use correct window.api to get ipc with electron

--- src/renderer/src/providers/ContentProvider.tsx
+++ src/renderer/src/providers/ContentProvider.tsx
@@ -13,107 +13,107 @@ dayjs.locale('pt-br');
 
 const ContentContext = createContext({} as ContentContextType);
 const initialWorker: WorkerData = {
-    queue: 0,
-    finished: 0,
-    chapters: 0,
-    images: 0,
-    bytes: 0,
-    last_update: dayjs().toNow(),
-    filter: '24h',
+  queue: 0,
+  finished: 0,
+  chapters: 0,
+  images: 0,
+  bytes: 0,
+  last_update: dayjs().toNow(),
+  filter: '24h'
 };
 
 export const ContentProvider = ({ children }: { children: React.ReactNode }) => {
-    const [status, setStatus] = useState<StatusType>('paused');
-    const [worker, setWorker] = useState<WorkerData>(initialWorker);
-    const [queue, setQueue] = useState<QueueData>({ current: null, queue: [], finished: [] });
-    const [{ access_token }, setCookie] = useCookies(['access_token']);
-    const [isAddQueueOpen, setIsAddQueueOpen] = useState(false);
-    const { data: user, refetch } = useQuery({
-        queryKey: ['user', `Bearer ${access_token}`],
-        queryFn: () =>
-            getUser(access_token).then(user => {
-                if (!user) {
-                    toast.error('Seu login expirou! Por favor, logue novamente.');
-                    logoff();
-                }
-                return user;
-            }),
-        enabled: !!access_token,
-    });
-
-    const changeStatus = (status: StatusType) => setStatus(status);
-    const logoff = useCallback(() => {
-        refetch();
-        setCookie('access_token', '', { path: '/' });
-        setStatus('paused');
-        if (window.electron) window.electron.removeUsers();
-        window.location.href = '/';
-    }, [refetch, setCookie]);
-    const changeWorkerFilter = (filter: WorkerFilter) =>
-        setWorker(c => ({
-            ...c,
-            filter,
-        }));
-    const refreshQueue = () => {
-        if (window.electron) {
-            window.electron.getQueue().then(setQueue);
+  const [status, setStatus] = useState<StatusType>('paused');
+  const [worker, setWorker] = useState<WorkerData>(initialWorker);
+  const [queue, setQueue] = useState<QueueData>({ current: null, queue: [], finished: [] });
+  const [{ access_token }, setCookie] = useCookies(['access_token']);
+  const [isAddQueueOpen, setIsAddQueueOpen] = useState(false);
+  const { data: user, refetch } = useQuery({
+    queryKey: ['user', `Bearer ${access_token}`],
+    queryFn: () =>
+      getUser(access_token).then((user) => {
+        if (!user) {
+          toast.error('Seu login expirou! Por favor, logue novamente.');
+          logoff();
         }
-        toast.error('A atualização da fila falhou!');
-    };
-    const closeAddQueue = () => setIsAddQueueOpen(false);
-    const openAddQueue = () => setIsAddQueueOpen(true);
+        return user;
+      }),
+    enabled: !!access_token
+  });
 
-    useEffect(() => {
-        if (window.electron) {
-            window.electron.getUser().then((user: User) => {
-                if (user) {
-                    setCookie('access_token', user.token, { path: '/' });
-                    refetch();
-                } else logoff();
-            });
-        }
-    }, [logoff, refetch, setCookie]);
+  const changeStatus = (status: StatusType) => setStatus(status);
+  const logoff = useCallback(() => {
+    refetch();
+    setCookie('access_token', '', { path: '/' });
+    setStatus('paused');
+    if (window.api) window.api.removeUsers();
+    window.location.href = '/';
+  }, [refetch, setCookie]);
+  const changeWorkerFilter = (filter: WorkerFilter) =>
+    setWorker((c) => ({
+      ...c,
+      filter
+    }));
+  const refreshQueue = () => {
+    if (window.api) {
+      window.api.getQueue().then(setQueue);
+    }
+    toast.error('A atualização da fila falhou!');
+  };
+  const closeAddQueue = () => setIsAddQueueOpen(false);
+  const openAddQueue = () => setIsAddQueueOpen(true);
 
-    return (
-        <ContentContext.Provider
-            value={{
-                user,
-                logoff,
-                status,
-                changeStatus,
-                worker,
-                changeWorkerFilter,
-                queue,
-                refreshQueue,
-                access_token,
-                closeAddQueue,
-                openAddQueue,
-                isAddQueueOpen,
-            }}
-        >
-            {children}
-        </ContentContext.Provider>
-    );
+  useEffect(() => {
+    if (window.api) {
+      window.api.getUser().then((user: User) => {
+        if (user) {
+          setCookie('access_token', user.token, { path: '/' });
+          refetch();
+        } else logoff();
+      });
+    }
+  }, [logoff, refetch, setCookie]);
+
+  return (
+    <ContentContext.Provider
+      value={{
+        user,
+        logoff,
+        status,
+        changeStatus,
+        worker,
+        changeWorkerFilter,
+        queue,
+        refreshQueue,
+        access_token,
+        closeAddQueue,
+        openAddQueue,
+        isAddQueueOpen
+      }}
+    >
+      {children}
+    </ContentContext.Provider>
+  );
 };
 
 // eslint-disable-next-line react-refresh/only-export-components
 export const useContent = () => {
-    const context = useContext(ContentContext);
-    if (!context) throw new Error('useContent must be used in a ContentProvider');
-    return context;
+  const context = useContext(ContentContext);
+  if (!context) throw new Error('useContent must be used in a ContentProvider');
+  return context;
 };
 
 interface ContentContextType {
-    user: UserEntity | undefined;
-    logoff: () => void;
-    status: StatusType;
-    changeStatus: (status: StatusType) => void;
-    worker: WorkerData;
-    changeWorkerFilter: (filter: WorkerFilter) => void;
-    queue: QueueData;
-    refreshQueue: () => void;
-    access_token?: string;
-    closeAddQueue: () => void;
-    openAddQueue: () => void;
-    isAddQueueOpen: boolean;
+  user: UserEntity | undefined;
+  logoff: () => void;
+  status: StatusType;
+  changeStatus: (status: StatusType) => void;
+  worker: WorkerData;
+  changeWorkerFilter: (filter: WorkerFilter) => void;
+  queue: QueueData;
+  refreshQueue: () => void;
+  access_token?: string;
+  closeAddQueue: () => void;
+  openAddQueue: () => void;
+  isAddQueueOpen: boolean;
 }
