b0c1300fd74862bba940f812036f3758d2daf23f
Vitu2002 <contact@vitu2002.dev>
2025-03-10 16:36:23 +0100

    feat(context): add react-query provider

--- src/renderer/src/main.tsx
+++ src/renderer/src/main.tsx
@@ -7,14 +7,17 @@ import App from './App';
 
 import '@styles/_global.scss';
 import '@styles/_toastify.scss';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 axios.defaults.baseURL = import.meta.env.VITE_API_URI;
 
 createRoot(document.getElementById('root')!).render(
-    <StrictMode>
-        <ContentProvider>
-            <App />
-            <ToastContainer autoClose={3000} theme="dark" pauseOnHover={true} />
-        </ContentProvider>
-    </StrictMode>
+  <StrictMode>
+    <QueryClientProvider client={new QueryClient()}>
+      <ContentProvider>
+        <App />
+        <ToastContainer autoClose={3000} theme="dark" pauseOnHover={true} />
+      </ContentProvider>
+    </QueryClientProvider>
+  </StrictMode>
 );
